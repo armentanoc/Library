b4aa360a45a890d02ab59fceeb966957c474f1e7
armentanoc <armentanocarolina@gmail.com>
2023-12-05 06:39:46 -0300

    Add project files.

---
+++ Library.ManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.ManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
